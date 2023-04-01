150ae15b7a232e2c76aba8572d2d6abc89bf46aa
Franck Stephane Ndzomga <ndzomgafs@gmail.com>
2023-04-01 04:58:23 +0200

    first usage of langchain

---
+++ app.js
@@ -0,0 +1,15 @@
+import { OpenAI } from "langchain";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+const model = new OpenAI({
+  openAIApiKey: process.env.OPENAI_API_KEY,
+  temperature: 0.9,
+});
+
+const res = await model.call(
+  "What would be a good company name a company that makes colorful socks?"
+);
+console.log(res);
