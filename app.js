463e8bfb1caacbafa2109d3299c2e30aac588511
Franck Stephane Ndzomga <ndzomgafs@gmail.com>
2023-04-01 05:17:33 +0200

    hiding api keys

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-import { OpenAI } from "langchain";
+import { OpenAI } from "langchain/llms";
+import { PromptTemplate } from "langchain/prompts";
+import { LLMChain } from "langchain/chains";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-
-const model = new OpenAI({
-  openAIApiKey: process.env.OPENAI_API_KEY,
-  temperature: 0.9,
+const model = new OpenAI({ openAIApiKey: process.env.OPENAI_API_KEY, temperature: 0.9 });
+const template = "What is a good name for a company that makes {product}?";
+const prompt = new PromptTemplate({
+  template: template,
+  inputVariables: ["product"],
 });
 
-const res = await model.call(
-  "What would be a good company name a company that makes colorful socks?"
-);
+const chain = new LLMChain({ llm: model, prompt: prompt });
+
+const res = await chain.call({ product: "colorful socks" });
 console.log(res);
