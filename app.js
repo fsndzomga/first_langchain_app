e532d74a473ef7c15a13f6246ec8bb6f542eed99
Franck Stephane Ndzomga <ndzomgafs@gmail.com>
2023-04-01 08:27:26 +0200

    playing with langchain agents

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-import { OpenAI } from "langchain/llms";
-import { PromptTemplate } from "langchain/prompts";
-import { LLMChain } from "langchain/chains";
+import { OpenAI } from "langchain";
+import { initializeAgentExecutor } from "langchain/agents";
+import { SerpAPI, Calculator } from "langchain/tools";
+
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const model = new OpenAI({ openAIApiKey: process.env.OPENAI_API_KEY, temperature: 0.9 });
-const template = "What is a good name for a company that makes {product}?";
-const prompt = new PromptTemplate({
-  template: template,
-  inputVariables: ["product"],
-});
+const tools = [new SerpAPI(), new Calculator()];
 
-const chain = new LLMChain({ llm: model, prompt: prompt });
+const executor = await initializeAgentExecutor(
+  tools,
+  model,
+  "zero-shot-react-description"
+);
+console.log("Loaded agent.");
 
-const res = await chain.call({ product: "colorful red socks" });
-console.log(res);
+const input =
+  "who is barack obama ?" +
+  " What is his age?";
+console.log(`Executing with input "${input}"...`);
+
+const result = await executor.call({ input });
+
+console.log(`Got output ${result.output}`);
